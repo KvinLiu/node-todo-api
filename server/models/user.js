8592c7b590bd9b80b0be72e024a0bde9b18619b9
Kevin Liu <kevin07liu@gmail.com>
2017-08-21 20:37:13 +0800

    Refine User model and add POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,40 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+// {
+// 	'email': 'kevin@gamil.com',
+// 	'password': 'jiofsanfdsajkofio',
+// 	'tokens': [{
+// 	  access: 'auth',
+// 	  token: 'fiodsanfioejfnkdjsoa;foen'
+//   }]
+// }
 
 const User = mongoose.model('User', {
 	email: {
 		type: String,
 		required: true,
 		minlength: 1,
-		trim: true
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+		  message: '{VALUE} is not a valid email'
+		},
+	  password: {
+			type: String,
+		  require: true,
+		  minlength: 6
+	  },
+	  tokens: [{
+			access: {
+				type: String,
+				required: true
+			},
+		  token: {
+				type: String,
+			  required: true
+		  }
+	  }]
 	}
 });
 
