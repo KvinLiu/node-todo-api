c810067efe880170e1a016066c32158b1e81aab7
Kevin Liu <kevin07liu@gmail.com>
2017-08-22 08:16:28 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,4 +1,5 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 // var length = hash.length;
@@ -26,15 +27,28 @@ const {SHA256} = require('crypto-js');
 // } else {
 // 	console.log('Data was changed. Do not trust!');
 // }
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+//
+// var data = {
+// 	id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+var password = '123abc!';
 
-var data = {
-	id: 10
-}
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 	  console.log(hash);
+//   });
+// })
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$BLhCSacwwIIpvPqOtDIafexOqPzbKRo0X6iKAdptHNiKOggtiu.yi';
 
-
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
