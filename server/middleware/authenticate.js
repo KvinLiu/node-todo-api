1dd4179b08ed8111e1265e0b11d5163fdc92892e
Kevin Liu <kevin07liu@gmail.com>
2017-08-22 16:14:26 +0800

    Add DELETE /users/me/token route

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -5,15 +5,15 @@ var authenticate = (req, res, next) => {
 
 	User.findByToken(token)
 		.then((user) => {
-		if (!user) {
-		return Promise.reject();
-	}
-	req.user = user;
-	req.token = token;
-	next();
-}).catch((e) => {
-		res.status(401).send();
-});
+		  if (!user) {
+		    return Promise.reject();
+	    }
+	    req.user = user;
+	    req.token = token;
+	    next();
+    }).catch((e) => {
+		    res.status(401).send();
+    });
 };
 
 module.exports = {authenticate};
